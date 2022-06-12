d39040b0bfe671a1ce68f31d9284494be9eee111
mccstrategymarketing.com.ua <mccstrategymarketing.com.ua>
2022-06-12 22:28:55 +0300

    Prepared clear template

---
+++ src/Components/Container/Container.jsx
@@ -0,0 +1,14 @@
+import "./clear.scss";
+import "./index.scss";
+
+export default function Container() {
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+    </div>
+  );
+}
